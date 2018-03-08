e8279a6942e34ff415dd672992fa841705691e51
Judzhin Miles <info@msbios.com>
2018-03-08 22:47:29 +0200

    Intermedaite commit

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,19 @@ const sass = require('gulp-sass');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
-    return gulp.src(["scss/*.scss"]).pipe(sass())
-        .pipe(gulp.dest("dist/css"))
+    return gulp.src(["src/scss/*.scss"]).pipe(sass())
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 // Move JS Files to src/js
+// Move the javascript files into our /src/js folder
 gulp.task('js', function() {
     return gulp.src([
-        'node_modules/bootstrap/js/dist/*.js',
-        'src/*.js'
-
-        // 'node_modules/popper.js/dist/umd/popper.min.js'
-    ])
-        .pipe(gulp.dest("dist/js"))
+        'node_modules/bootstrap/dist/js/bootstrap.min.js', 
+        'node_modules/jquery/dist/jquery.min.js', 
+        'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
 });
 
@@ -30,20 +29,8 @@ gulp.task('serve', ['sass'], function() {
     browserSync.init({
         server: "./src"  
     });
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch(["src/scss/*.scss"], ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-// Move Fonts to src/fonts
-gulp.task('fonts', function() {
-  return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('src/fonts'))
-})
-
-// Move Font Awesome CSS to src/css
-gulp.task('fa', function() {
-  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest('src/css'))
-})
-
-gulp.task('default', ['js','serve', 'fa', 'fonts']);
+gulp.task('default', ['js','serve']);
